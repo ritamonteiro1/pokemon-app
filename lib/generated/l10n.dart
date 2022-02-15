7d5babf8f5acacc31e5772ef87fb1c44ce48373b
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2022-02-15 17:23:07 -0300

    add locales en, en_US, es and pt_BR to intl

--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -57,6 +57,9 @@ class AppLocalizationDelegate extends LocalizationsDelegate<S> {
   List<Locale> get supportedLocales {
     return const <Locale>[
       Locale.fromSubtags(languageCode: 'en', countryCode: 'US'),
+      Locale.fromSubtags(languageCode: 'en'),
+      Locale.fromSubtags(languageCode: 'es'),
+      Locale.fromSubtags(languageCode: 'pt', countryCode: 'BR'),
     ];
   }
 
