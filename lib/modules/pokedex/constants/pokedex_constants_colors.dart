1412effc259d638294088b3ef77750fa2a85896e
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2022-02-15 17:50:42 -0300

    create PokedexConstantsColors

---
+++ lib/modules/pokedex/constants/pokedex_constants_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class PokedexConstantsColors {
+  static const rock = Color(0xFFB69E31);
+  static const ghost = Color(0XFF70559B);
+  static const steel = Color(0XFFB7B9D0);
+  static const water = Color(0XFFB7B9D0);
+  static const crass = Color(0XFF74CB48);
+  static const psychic = Color(0XFFFB5584);
+  static const ice = Color(0XFF9AD6DF);
+  static const dark = Color(0XFF75574C);
+  static const fairy = Color(0XFFE69EAC);
+  static const normal = Color(0XFFAAA67F);
+  static const fighting = Color(0XFFAAA67F);
+  static const flying = Color(0XFFA891EC);
+  static const poison = Color(0XFFA43E9E);
+  static const ground = Color(0XFFDEC16B);
+  static const bug = Color(0XFFA7B723);
+  static const fire = Color(0XFFF57D31);
+  static const electric = Color(0XFFF9CF30);
+  static const dragon = Color(0XFF7037FF);
+  static const darkGray = Color(0XFF212121);
+  static const mediumGray = Color(0XFF666666);
+  static const lightGray = Color(0XFFE0E0E0);
+  static const white = Color(0XFFFFFFFF);
+  static const background = Color(0XFFF7F7F7);
+}
