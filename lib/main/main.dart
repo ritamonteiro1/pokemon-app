60a2dde88d4d574e2dbc2e3ffc9de8201cacd9ae
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2022-02-15 16:51:15 -0300

    import packages that will be used

---
+++ lib/main/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import '../pokemon_app/pokemon_app.dart';
+
+void main() {
+  runApp(const PokemonApp());
+}
---
+++ lib/pokemon_app/pokemon_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PokemonApp extends StatelessWidget {
+  const PokemonApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+      );
+}
