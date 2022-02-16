aefa721e6f28cbb9e774ed1c1ad8827ce13f9143
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2022-02-16 19:17:26 -0300

    add images in PokemonConstantsImages

--- lib/modules/pokemon/constants/pokemon_constants_images.dart
+++ lib/modules/pokemon/constants/pokemon_constants_images.dart
@@ -1,3 +1,11 @@
 class PokemonConstantsImages {
   static const logoIoasys = 'assets/images/logo_ioasys.png';
+  static const backArrow = 'assets/images/back_arrow.png';
+  static const close = 'assets/images/close.png';
+  static const downArrow = 'assets/images/down_arrow.png';
+  static const reload = 'assets/images/reload.png';
+  static const rightArrow = 'assets/images/right_arrow.png';
+  static const rulerHeight = 'assets/images/ruler_height.png';
+  static const search = 'assets/images/search.png';
+  static const weightScales = 'assets/images/weight_scales.png';
 }
--- lib/modules/pokemon/module/pokemon_module.dart
+++ lib/modules/pokemon/module/pokemon_module.dart
@@ -1,13 +1,36 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:hive/hive.dart';
 
 import '../constants/pokemon_constants_routes.dart';
+import '../data/cache/data_source/pokemon_cache_data_source.dart';
+import '../data/cache/data_source/pokemon_cache_data_source_impl.dart';
+import '../data/remote/data_source/pokemon_remote_data_source.dart';
+import '../data/remote/data_source/pokemon_remote_data_source_impl.dart';
+import '../data/repository_impl/pokemon_repository_impl.dart';
+import '../domain/repository/pokemon_repository.dart';
+import '../domain/use_case/get_pokemon_list_use_case.dart';
+import '../domain/use_case/get_pokemon_list_use_case_impl.dart';
 import '../presentation/favorite_pokemon_list/favorite_pokemon_list_screen.dart';
 import '../presentation/pokemon_details/pokemon_details_screen.dart';
 import '../presentation/pokemon_list/pokemon_list_screen.dart';
+import '../presentation/pokemon_list/pokemon_list_store.dart';
 
 class PokemonModule extends Module {
   @override
-  List<Bind> get binds => [];
+  List<Bind> get binds => [
+        Bind.lazySingleton((i) => Dio()),
+        Bind.lazySingleton((i) => Hive),
+        Bind.lazySingleton<PokemonCacheDataSource>(
+            (i) => PokemonCacheDataSourceImpl(hive: i())),
+        Bind.lazySingleton<PokemonRemoteDataSource>(
+            (i) => PokemonRemoteDataSourceImpl(dio: i())),
+        Bind.lazySingleton<PokemonRepository>(
+            (i) => PokemonRepositoryImpl(pokemonRemoteDataSource: i())),
+        Bind.lazySingleton<GetPokemonListUseCase>(
+            (i) => GetPokemonListUseCaseImpl(pokemonRepository: i())),
+        Bind.factory((i) => PokemonListStore(getPokemonListUseCase: i())),
+      ];
 
   @override
   List<ModularRoute> get routes => [
