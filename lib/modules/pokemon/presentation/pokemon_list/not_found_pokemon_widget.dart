a365a4cc16db043351556504767541ffef822134
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2022-02-17 02:08:17 -0300

    create not_found_pokemon_widget.dart

--- lib/modules/pokemon/presentation/pokemon_list/not_found_pokemon_widget.dart
+++ lib/modules/pokemon/presentation/pokemon_list/not_found_pokemon_widget.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import '../../../../generated/l10n.dart';
+import '../../constants/pokemon_constants_colors.dart';
 
 class NotFoundPokemonWidget extends StatelessWidget {
   const NotFoundPokemonWidget({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  Widget build(BuildContext context) => Padding(
+      padding: const EdgeInsets.only(
+        top: 42,
+      ),
+      child: Column(
+        children: [
+          Text(
+            S.of(context).messageAlertOps,
+            style: const TextStyle(
+              color: PokemonConstantsColors.grey,
+              fontSize: 148,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text(
+            S.of(context).messageNotFoundPokemon,
+            style: const TextStyle(
+              color: PokemonConstantsColors.grey,
+              fontSize: 30,
+            ),
+          ),
+        ],
+      ));
 }
