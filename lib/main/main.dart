c7e57be5f79514837ef00f1a46030a111d5e923c
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2022-02-21 14:07:54 -0300

    add ensureInitialized in main.dart

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+
+import '../modules/pokemon/data/cache/model/pokemon_cache.dart';
+import '../modules/pokemon/data/cache/model/stat_cache.dart';
 import '../pokedex_app/pokemon_app.dart';
 import '../pokedex_module/pokedex_module.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Hive
+    ..init((await getApplicationDocumentsDirectory()).path)
+    ..registerAdapter<PokemonCache>(PokemonCacheAdapter())
+    ..registerAdapter<StatCache>(StatCacheAdapter());
+
   runApp(ModularApp(
     module: PokedexModule(),
     child: const PokedexApp(),
