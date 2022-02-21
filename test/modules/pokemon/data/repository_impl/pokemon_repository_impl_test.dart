b7b58db22dbe6c002ba63e9b70059711e7592d43
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2022-02-21 11:53:43 -0300

    create get_pokemon_list_use_case_impl_test.dart

--- test/modules/pokemon/data/repository_impl/pokemon_repository_impl_test.dart
+++ test/modules/pokemon/data/repository_impl/pokemon_repository_impl_test.dart
@@ -26,15 +26,54 @@ void main() {
         'WHEN request is successfully '
         'THEN it should return an Pokemon Model list', () async {
       when(mockPokemonRemoteDataSource.getPokemonList())
-          .thenAnswer((_) async => _getSuccessfulPokemonModelMock());
+          .thenAnswer((_) async => _getSuccessfulPokemonModelListMock());
       final pokemonList = await pokemonRepositoryImpl.getPokemonList();
-      expect(pokemonList, _getSuccessfulPokemonModelMock());
+      expect(pokemonList, _getSuccessfulPokemonModelListMock());
       verify(mockPokemonRemoteDataSource.getPokemonList()).called(1);
+    });
+    test(
+        'WHEN request is fail '
+        'THEN it should throw an exception', () async {
+      when(mockPokemonRemoteDataSource.getPokemonList()).thenThrow(Exception());
+      expect(() => pokemonRepositoryImpl.getPokemonList(), throwsException);
+    });
+  });
+  group('GIVEN a call on getPokemonTyped', () {
+    test(
+        'WHEN request is successfully '
+        'THEN it should return an Pokemon Model', () async {
+      when(mockPokemonRemoteDataSource.getPokemonTyped(any))
+          .thenAnswer((_) async => _getSuccessfulPokemonModelMock());
+      final pokemon = await pokemonRepositoryImpl.getPokemonTyped('pokemon 1');
+      expect(pokemon, _getSuccessfulPokemonModelMock());
+      verify(mockPokemonRemoteDataSource.getPokemonTyped(any)).called(1);
+    });
+    test(
+        'WHEN request is fail '
+        'THEN it should throw an exception', () async {
+      when(mockPokemonRemoteDataSource.getPokemonTyped(any))
+          .thenThrow(Exception());
+      expect(() => pokemonRepositoryImpl.getPokemonTyped('pokemon 1'),
+          throwsException);
     });
   });
 }
 
-List<PokemonModel> _getSuccessfulPokemonModelMock() => const <PokemonModel>[
+PokemonModel _getSuccessfulPokemonModelMock() => const PokemonModel(
+    abilityList: <String>['1', '2'],
+    height: 10,
+    id: 1,
+    name: 'pokemon 1',
+    statList: <StatModel>[
+      StatModel(base: 1, name: 'name 1'),
+      StatModel(base: 2, name: 'name 2'),
+    ],
+    typeList: <String>['1', '2'],
+    weight: 10,
+    image:
+        'https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/1.svg');
+
+List<PokemonModel> _getSuccessfulPokemonModelListMock() => const <PokemonModel>[
       PokemonModel(
           abilityList: <String>['1', '2'],
           height: 10,
---
+++ test/modules/pokemon/domain/use_case/get_pokemon_list_use_case_impl_test.dart
@@ -0,0 +1,64 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:pokedex_app/modules/pokemon/domain/model/pokemon/pokemon_model.dart';
+import 'package:pokedex_app/modules/pokemon/domain/model/pokemon/stat_model.dart';
+import 'package:pokedex_app/modules/pokemon/domain/repository/pokemon_repository.dart';
+import 'package:pokedex_app/modules/pokemon/domain/use_case/get_pokemon_list_use_case.dart';
+import 'package:pokedex_app/modules/pokemon/domain/use_case/get_pokemon_list_use_case_impl.dart';
+
+import 'get_pokemon_list_use_case_impl_test.mocks.dart';
+
+@GenerateMocks([PokemonRepository])
+void main() {
+  late MockPokemonRepository mockPokemonRepository;
+  late GetPokemonListUseCase getPokemonListUseCase;
+  setUpAll(() {
+    mockPokemonRepository = MockPokemonRepository();
+    getPokemonListUseCase =
+        GetPokemonListUseCaseImpl(pokemonRepository: mockPokemonRepository);
+  });
+  setUp(() {
+    reset(mockPokemonRepository);
+  });
+  group('GIVEN a call', () {
+    test(
+        'WHEN request is successfully '
+        'THEN it should return an PokemonModel list', () async {
+      when(mockPokemonRepository.getPokemonList())
+          .thenAnswer((_) async => _getSuccessfulPokemonModelListMock());
+      final pokemonList = await getPokemonListUseCase.call();
+      expect(pokemonList, _getSuccessfulPokemonModelListMock());
+      verify(mockPokemonRepository.getPokemonList());
+    });
+  });
+}
+
+List<PokemonModel> _getSuccessfulPokemonModelListMock() => const <PokemonModel>[
+      PokemonModel(
+          abilityList: <String>['1', '2'],
+          height: 10,
+          id: 1,
+          name: 'pokemon 1',
+          statList: <StatModel>[
+            StatModel(base: 1, name: 'name 1'),
+            StatModel(base: 2, name: 'name 2'),
+          ],
+          typeList: <String>['1', '2'],
+          weight: 10,
+          image:
+              'https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/1.svg'),
+      PokemonModel(
+          abilityList: <String>['3', '4'],
+          height: 20,
+          id: 2,
+          name: 'pokemon 2',
+          statList: <StatModel>[
+            StatModel(base: 3, name: 'name 3'),
+            StatModel(base: 4, name: 'name 4'),
+          ],
+          typeList: <String>['1', '2'],
+          weight: 20,
+          image:
+              'https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/2.svg'),
+    ];
