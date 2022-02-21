c770a73bf07d0544f123c12085b59d7e15067d09
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2022-02-21 15:27:18 -0300

    finished pokemon_cache_data_source_impl.dart

--- lib/modules/pokemon/data/cache/data_source/pokemon_cache_data_source_impl.dart
+++ lib/modules/pokemon/data/cache/data_source/pokemon_cache_data_source_impl.dart
@@ -1,7 +1,10 @@
 import 'package:hive/hive.dart';
-import 'package:pokedex_app/modules/pokemon/data/mapper/model_to_cache.dart';
 
+import '../../../domain/exception/empty_favorite_pokemon_list_exception.dart';
 import '../../../domain/model/pokemon/pokemon_model.dart';
+import '../../mapper/cache_to_model.dart';
+import '../../mapper/model_to_cache.dart';
+import '../model/pokemon_cache.dart';
 import 'pokemon_cache_data_source.dart';
 
 class PokemonCacheDataSourceImpl implements PokemonCacheDataSource {
@@ -15,18 +18,29 @@ class PokemonCacheDataSourceImpl implements PokemonCacheDataSource {
 
   @override
   Future<void> addFavoritePokemon(PokemonModel pokemonModel) async {
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<List<PokemonModel>> getFavoritePokemonList() async {
-    throw UnimplementedError();
+    final pokemonCache = pokemonModel.toPokemonCache();
+    final box = await _hive.openBox(_favoritePokemonListKeyString);
+    await box.put(pokemonModel.id, pokemonCache);
   }
 
   @override
   Future<void> removeFavoritePokemon(PokemonModel pokemonModel) async {
     final pokemonCache = pokemonModel.toPokemonCache();
-    final box = await Hive.openBox(_favoritePokemonListKeyString);
-    await box.put(pokemonModel.id, pokemonCache);
+    final box = await _hive.openBox(_favoritePokemonListKeyString);
+    await box.delete(pokemonCache);
+  }
+
+  @override
+  Future<List<PokemonModel>> getFavoritePokemonList() async {
+    final box = await _hive.openBox(_favoritePokemonListKeyString);
+    final favoritePokemonCacheList =
+        List<PokemonCache>.from(box.get(_favoritePokemonListKeyInt));
+    final favoritePokemonModelList =
+        favoritePokemonCacheList.toPokemonModelList();
+    if (favoritePokemonModelList.isNotEmpty) {
+      return favoritePokemonModelList;
+    } else {
+      throw EmptyFavoritePokemonListException();
+    }
   }
 }
