6d98d5aba64a72fc46cdb1e5f72d4b171cf5ab0d
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2022-02-21 16:45:41 -0300

    finished FavoritePokemonListStore

--- lib/modules/pokemon/presentation/favorite_pokemon_list/favorite_pokemon_list_store.g.dart
+++ lib/modules/pokemon/presentation/favorite_pokemon_list/favorite_pokemon_list_store.g.dart
@@ -9,10 +9,27 @@ part of 'favorite_pokemon_list_store.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$FavoritePokemonListStore on _FavoritePokemonListStore, Store {
+  final _$favoritePokemonListStateAtom =
+      Atom(name: '_FavoritePokemonListStore.favoritePokemonListState');
+
+  @override
+  FavoritePokemonListState get favoritePokemonListState {
+    _$favoritePokemonListStateAtom.reportRead();
+    return super.favoritePokemonListState;
+  }
+
+  @override
+  set favoritePokemonListState(FavoritePokemonListState value) {
+    _$favoritePokemonListStateAtom
+        .reportWrite(value, super.favoritePokemonListState, () {
+      super.favoritePokemonListState = value;
+    });
+  }
+
   @override
   String toString() {
     return '''
-
+favoritePokemonListState: ${favoritePokemonListState}
     ''';
   }
 }
