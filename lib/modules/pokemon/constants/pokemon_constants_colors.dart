b134bc5f61e5fbccd9ef13db3d372689f43d2828
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2022-02-23 21:01:56 -0300

    change in PokemonConstantsColors

--- lib/modules/pokemon/constants/pokemon_constants_colors.dart
+++ lib/modules/pokemon/constants/pokemon_constants_colors.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
 
 class PokemonConstantsColors {
-  static const rock = Color(0xFFB69E31);
-  static const ghost = Color(0XFF70559B);
-  static const steel = Color(0XFFB7B9D0);
-  static const water = Color(0XFFB7B9D0);
-  static const crass = Color(0XFF74CB48);
-  static const psychic = Color(0XFFFB5584);
-  static const ice = Color(0XFF9AD6DF);
-  static const dark = Color(0XFF75574C);
-  static const fairy = Color(0XFFE69EAC);
-  static const normal = Color(0XFFAAA67F);
-  static const fighting = Color(0XFFAAA67F);
-  static const flying = Color(0XFFA891EC);
-  static const poison = Color(0XFFA43E9E);
-  static const ground = Color(0XFFDEC16B);
-  static const bug = Color(0XFFA7B723);
-  static const fire = Color(0XFFF57D31);
-  static const electric = Color(0XFFF9CF30);
-  static const dragon = Color(0XFF7037FF);
+  static const Map<String, Color> pokemons = {
+    'rock': Color(0xFFB69E31),
+    'ghost': Color(0XFF70559B),
+    'steel': Color(0XFFB7B9D0),
+    'water': Color(0XFFB7B9D0),
+    'crass': Color(0XFF74CB48),
+    'psychic': Color(0XFFFB5584),
+    'ice': Color(0XFF9AD6DF),
+    'dark': Color(0XFF75574C),
+    'fairy': Color(0XFFE69EAC),
+    'normal': Color(0XFFAAA67F),
+    'fighting': Color(0XFFAAA67F),
+    'flying': Color(0XFFA891EC),
+    'poison': Color(0XFFA43E9E),
+    'ground': Color(0XFFDEC16B),
+    'bug': Color(0XFFA7B723),
+    'fire': Color(0XFFF57D31),
+    'electric': Color(0XFFF9CF30),
+    'dragon': Color(0XFF7037FF)
+  };
   static const darkGray = Color(0XFF212121);
   static const mediumGray = Color(0XFF666666);
   static const lightGray = Color(0XFFE0E0E0);
