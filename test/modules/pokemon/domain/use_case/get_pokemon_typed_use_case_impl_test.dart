ce4276275ea9cd0cee34ccbf0f8c7f699a10e532
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2022-02-25 16:17:34 -0300

    add tests in get_pokemon_typed_use_case_impl_test.dart

--- test/modules/pokemon/domain/use_case/get_pokemon_typed_use_case_impl_test.dart
+++ test/modules/pokemon/domain/use_case/get_pokemon_typed_use_case_impl_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
@@ -31,8 +30,41 @@ void main() {
       expect(pokemon, _getSuccessfulPokemonModelMock());
       verify(mockPokemonRepository.getPokemonTyped(any));
     });
+    test(
+        'WHEN request is successfully and id typed pokemon equals 5'
+        'THEN it should return a PokemonModel with id equals 5', () async {
+      when(mockPokemonRepository.getPokemonTyped(any)).thenAnswer(
+          (_) async => _getSuccessfulPokemonModelWithIdEquals5Mock());
+      final pokemon = await getPokemonTypedUseCase.call('pokemon 5');
+      expect(pokemon.id, 5);
+      verify(mockPokemonRepository.getPokemonTyped(any));
+    });
+    test(
+        'WHEN request is fail '
+        'THEN it should  throw an exception', () async {
+      when(mockPokemonRepository.getPokemonTyped(any)).thenThrow(Exception());
+      expect(() => getPokemonTypedUseCase.call('pokemon 1'), throwsException);
+      verify(mockPokemonRepository.getPokemonTyped(any));
+    });
   });
 }
+
+PokemonModel _getSuccessfulPokemonModelWithIdEquals5Mock() => PokemonModel(
+      abilityList: const <String>['1', '2'],
+      height: 10,
+      id: 5,
+      name: 'pokemon 5',
+      statList: const <StatModel>[
+        StatModel(base: 1, name: 'name 1'),
+        StatModel(base: 2, name: 'name 2'),
+      ],
+      typeList: const <String>['1', '2'],
+      weight: 10,
+      image:
+          'https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/1.svg',
+      colorNameByFirstType: 'color',
+      description: 'description 1',
+    );
 
 PokemonModel _getSuccessfulPokemonModelMock() => PokemonModel(
       abilityList: const <String>['1', '2'],
