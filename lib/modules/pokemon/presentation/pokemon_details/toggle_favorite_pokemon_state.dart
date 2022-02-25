6ba1f19d26e951c7703eab1ea5b1b0e35d8cd153
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2022-02-25 01:37:04 -0300

    create ToggleFavoritePokemonState

---
+++ lib/modules/pokemon/presentation/pokemon_details/toggle_favorite_pokemon_state.dart
@@ -0,0 +1,7 @@
+abstract class ToggleFavoritePokemonState {}
+
+class SuccessfullyAddFavoritePokemon implements ToggleFavoritePokemonState {}
+
+class FailAddFavoritePokemon implements ToggleFavoritePokemonState {}
+
+class FailToggleFavoritePokemon implements ToggleFavoritePokemonState {}
