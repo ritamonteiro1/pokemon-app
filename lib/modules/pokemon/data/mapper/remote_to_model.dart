85c5b2d9e811f8d28611bdd6bbbec555131dee45
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2022-03-02 11:31:07 -0300

    create const nullIntResponse and nullStringResponse in remote_to_model.dart

--- lib/modules/pokemon/data/mapper/remote_to_model.dart
+++ lib/modules/pokemon/data/mapper/remote_to_model.dart
@@ -5,41 +5,47 @@ import '../remote/model/pokemon/details/pokemon_response.dart';
 import '../remote/model/pokemon/specie/specie_response.dart';
 
 PokemonModel convertToPokemonModelList(
-        PokemonResponse pokemonResponse, SpecieResponse specieResponse) =>
-    PokemonModel(
-      abilityList: pokemonResponse.abilities
-              ?.map(
-                (ability) => ability.ability?.name == null
-                    ? ' - '
-                    : ability.ability!.name!.capitalize(),
-              )
-              .toList() ??
-          [],
-      height: pokemonResponse.height ?? -1,
-      id: pokemonResponse.id,
-      name: pokemonResponse.name == null
-          ? ' - '
-          : pokemonResponse.name!.capitalize(),
-      statList: pokemonResponse.stats
-              ?.map(
-                (stat) => StatModel(
-                    name: stat.stat?.name ?? ' - ', base: stat.baseStat ?? -1),
-              )
-              .toList() ??
-          [],
-      typeList: pokemonResponse.types
-              ?.map(
-                (type) => type.type?.name == null
-                    ? ' - '
-                    : type.type!.name!.capitalize(),
-              )
-              .toList() ??
-          [],
-      weight: pokemonResponse.weight ?? -1,
-      image:
-          'https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${pokemonResponse.id}.svg',
-      colorNameByFirstType: pokemonResponse.types?[0].type?.name == null
-          ? ' - '
-          : pokemonResponse.types![0].type!.name!.capitalize(),
-      description: specieResponse.descriptionList?[9].description ?? ' - ',
-    );
+    PokemonResponse pokemonResponse, SpecieResponse specieResponse) {
+  const nullIntResponse = -1;
+  const nullStringResponse = ' - ';
+
+  return PokemonModel(
+    abilityList: pokemonResponse.abilities
+            ?.map(
+              (ability) => ability.ability?.name == null
+                  ? nullStringResponse
+                  : ability.ability!.name!.capitalize(),
+            )
+            .toList() ??
+        [],
+    height: pokemonResponse.height ?? nullIntResponse,
+    id: pokemonResponse.id,
+    name: pokemonResponse.name == null
+        ? nullStringResponse
+        : pokemonResponse.name!.capitalize(),
+    statList: pokemonResponse.stats
+            ?.map(
+              (stat) => StatModel(
+                  name: stat.stat?.name ?? nullStringResponse,
+                  base: stat.baseStat ?? nullIntResponse),
+            )
+            .toList() ??
+        [],
+    typeList: pokemonResponse.types
+            ?.map(
+              (type) => type.type?.name == null
+                  ? nullStringResponse
+                  : type.type!.name!.capitalize(),
+            )
+            .toList() ??
+        [],
+    weight: pokemonResponse.weight ?? nullIntResponse,
+    image:
+        'https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${pokemonResponse.id}.svg',
+    colorNameByFirstType: pokemonResponse.types?[0].type?.name == null
+        ? nullStringResponse
+        : pokemonResponse.types![0].type!.name!.capitalize(),
+    description:
+        specieResponse.descriptionList?[9].description ?? nullStringResponse,
+  );
+}
