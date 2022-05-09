9c7d8f67fbfaafe3b178a20978ed0c209b81501d
rita monteiro <ritaalvesmonteiro@gmail.com>
2022-05-09 16:41:03 -0300

    update flutter modular

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'di/pokedex_module.dart';
 import 'generated/l10n.dart';
 import 'modules/common/constants/pokedex_constants_colors.dart';
 import 'modules/common/constants/pokedex_constants_fonts.dart';
-import 'modules/common/constants/pokedex_constants_routes.dart';
 import 'modules/pokemon/data/cache/model/pokemon_cache.dart';
 import 'modules/pokemon/data/cache/model/stat_cache.dart';
 
@@ -32,22 +31,23 @@ class PokedexApp extends StatelessWidget {
   const PokedexApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData(
-      fontFamily: PokedexConstantsFonts.poppins,
-      primarySwatch: Colors.blue,
-      colorScheme: ColorScheme.fromSwatch().copyWith(
-        primary: PokedexConstantsColors.primaryColor,
-      ),
-    ),
-    localizationsDelegates: const [
-      S.delegate,
-      GlobalMaterialLocalizations.delegate,
-      GlobalWidgetsLocalizations.delegate,
-      GlobalCupertinoLocalizations.delegate,
-    ],
-    supportedLocales: S.delegate.supportedLocales,
-    initialRoute: PokedexConstantsRoutes.splashModule,
-  ).modular();
+  Widget build(BuildContext context) => MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          fontFamily: PokedexConstantsFonts.poppins,
+          primarySwatch: Colors.blue,
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: PokedexConstantsColors.primaryColor,
+          ),
+        ),
+        localizationsDelegates: const [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales,
+        routeInformationParser: Modular.routeInformationParser,
+        routerDelegate: Modular.routerDelegate,
+      );
 }
